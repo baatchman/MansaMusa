b3558585283c2e8127550603cb90839d5244bb12
QuerkyBren <robloxbren@gmail.com>
2019-07-07 23:41:08 +1000

    Slimmed down and removed human interface, moved it to debug/

--- Interface/main.py
+++ Interface/main.py
@@ -1,98 +1,43 @@
+#!/usr/bin/python
 
-#Importing modules
-import sys
-from cmd import Cmd
-import binance
-import json
-import unicodedata
-import os
+#Import modules
+import click
 from pathlib import Path
-from binance.client import Client
-from binance.enums import *
-import argparse
+import os
+import sys
 
-#Importing scripts
+#Import scripts
 sys.path.insert(0, 'code/')
 import market
 
-class Prompt(Cmd):
+#Setup command line arguments
+@click.command()
+@click.option('-a', type=click.STRING, help='Indicates where your account file is.')
+@click.option('-t', type=click.Choice(['buy','sell']), help='Your choice of transaction')
+@click.option('-o', type=click.Choice(['market','limit']), help='Your choice of order')
+@click.option('-q', type=click.INT, help='How much of the currency you would like to buy/sell')
+@click.option('-p', type=clic9k.INT, help='What price you will bargain for.')
+@click.option('-c', type=click.STRING, help='The crypto token of your choice.')
 
-	def do_hello(self, args):
-		"""Says hello. If you provide a name, it will greet you with it."""
-		if len(args) == 0:
-			name = 'stranger'
-		else:
-			name = args
-			print("Hello, %s" % name)
+#Function
+def init(a, t, o, q, p, c):
+    #Load the account file
+    click.echo("Loading acc9ounts...")
+    accfile = Path(a)
+    if accfile.is_file():
+        if os.path.getsize(a) == 0:
+            click.echo("ERROR: No accounts in the account file, please put them in.")
+            sys.exit()
+        click.echo("Accounts loaded")
+        #Transition to the market interface.
+        click.echo("Setting up exchange interface")
+        m = market.Market()
+        m.xch(t, o, c, q, p)
+    else:
+        click.echo("ERROR: The specified file does not exist.")
+        sys.exit()
 
-	def do_quit(self, args):
-		"""Quits the program."""
-		print ("Quitting.")
-		raise SystemExit
-
-	def do_addAccount(self, args):
-		"""Adds a new account."""
-		m = Main()
-		m.setupAccounts()
-
-	def do_order(self, *args):
-		"""Places an order.\n
-		Arguments in order:\n
-		Buy or Sell option.\n
-		Market or Limit option.\n
-		The crypto symbol.\n
-		The quantity of the order.\n
-		The price of the order.\n"""
-		
-		args = args
-		m = market.Market()
-		m.xch(*args)
-
-
-class Main():
-
-	def addAccount(self):
-		a_keyamount = input("How many accounts would you like to add, sir?")
-		a_name = ""
-		a_key = ""
-
-		f=open("data/accounts.txt", "a")
-		for i in range(a_keyamount):
-			a_key = raw_input("What is the API Key for this account, sir? > ")
-			a_name = raw_input("What would you like to call this account, sir? > ")
-			f.write(name + "\n" + str(key) + "\n")
-
-
-		
-	def setupAccounts(self):
-		print("It seems as though you don't have your accounts set up, I'll get to doing that right now. I just need to ask you some questions :-)")
-		keyamount = input("How many accounts do you have sir? > ")
-		key = ""
-		name = ""
-		
-		f=open("data/accounts.txt","w+")
-		for i in range(keyamount):
-			key = raw_input("What is the API Key for the account, sir? > ")
-			name = raw_input("What would you like to call this account, sir? > ")
-			f.write(name + "\n" + str(key) + "\n")
-		f.close()
-		print("Thank you sir, now we can set up the program.")
-
-	def initAccounts(self):
-		print("Loading Accounts...")
-		accfile = Path("data/accounts.txt")
-		if accfile.is_file():
-			if os.path.getsize("data/accounts.txt") == 0:
-				print("No accounts in the account file, let's fix this shall we?")
-				os.remove("data/accounts.txt")
-				self.setupAccounts()
-			prompt = Prompt()
-			prompt.prompt = '> '
-			prompt.cmdloop('CRYPTOANAYLISIS: PREDICTOR')
-		else:
-			self.setupAccounts()
-
+    
 
 if __name__ == '__main__':
-	m = Main()
-	m.initAccounts()
+    init()
