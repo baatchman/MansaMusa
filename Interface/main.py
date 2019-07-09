0d4ff13ce16a32c13234d7e06662f752dc35681b
QuerkyBren <robloxbren@gmail.com>
2019-07-09 13:34:58 +1000

    Cleaned up code, moved --setup to main function.

--- Interface/main.py
+++ Interface/main.py
@@ -24,54 +24,59 @@ import setupaccounts
 @click.option('-q', type=click.FLOAT, help='How much of the currency you would like to buy/sell')
 @click.option('-p', type=click.FLOAT, help='What price you will bargain for.')
 @click.option('-c', type=click.STRING, help='The crypto token of your choice.')
-
-#Function
-def init(a, t, o, q, p, c):
-    #Load the accounts
-    click.echo("Loading accounts...")
-    time.sleep(1)
-    accfile = Path(a)
-    #Find the account file
-    click.echo("Detecting Account file...")
-    time.sleep(1)
-    if accfile.is_file():
-        if os.path.getsize(a) == 0:
-            click.echo("ERROR: No accounts in the account file, please put them in.")
-            sys.exit()
-        click.echo("Account file detected.")
-        time.sleep(2)
-        #Find the number of lines in file
-        num_lines = 0
-        with open(accfile, "r") as f:
-            for line in f:
-                num_lines += 1
-        #Put account keys into 2d array
-        duo = 0
-        keyarray = []
-        bruh = open(accfile, "r")
-        while duo != num_lines / 2:
-             p_key = f.readline()
-             s_key = f.readline()
-             keyarray.insert(duo, [p_key, s_key])
-             duo += 1
-        click.echo("Accounts loaded.")
-        time.sleep(1)       
-        #Transition to the market interface.
-        click.echo("Setting up exchange interface")
-        time.sleep(1)
-        client = Client(keyarray[0][0], keyarray[0][1])
-        m = market.Market()
-        m.xch(t, o, c, q, p, keyarray[0][0], keyarray[0][1])
-    else:
-        click.echo("ERROR: The specified file does not exist.")
-        sys.exit()
-
-@click.command()
 @click.option('--setup', type=click.BOOL, default='false', help="Sends you to a user friendly program to set your account file, use '--setup true' to do this.")
 
-def setupAccounts(setup):
-    sa = setupaccounts
-    sa.main()
+#Main function
+def init(a, t, o, q, p, c, setup):
+    if setup == True:
+        setupaccounts.main()
+    else:
+        #Load the accounts
+        click.echo("Loading accounts...")
+        time.sleep(1)
+        accfile = Path(a)
+        #Find the account file
+        click.echo("Detecting Account file...")
+        time.sleep(1)
+        if accfile.is_file():
+            if os.path.getsize(a) == 0:
+                click.echo("ERROR: No accounts in the account file, please put them in.")
+                sys.exit()
+            click.echo("Account file detected.")
+            time.sleep(2)
+            #Find the number of lines in file
+            num_lines = 0
+            with open(accfile, "r") as f:
+                for line in f:
+                    num_lines += 1
+            #Put account keys into 2d array
+            duo = 0
+            keyarray = []
+            bruh = open(accfile, "r")
+            while duo != num_lines / 2:
+                p_key = bruh.readline()
+                s_key = bruh.readline()
+                p_key_clean = p_key.rstrip("\n\r")
+                s_key_clean = s_key.rstrip("\n\r")
+                keyarray.insert(duo, [p_key_clean, s_key_clean])
+                duo += 1
+            click.echo("Accounts loaded.")
+            time.sleep(1)       
+            #Transition to the market interface.
+            click.echo("Setting up exchange interface")
+            time.sleep(1)
+            client = Client(keyarray[0][0], keyarray[0][1])
+            m = market.Market()
+            m.xch(t, o, c, q, p, keyarray[0][0], keyarray[0][1])
+        else:
+            click.echo("ERROR: The specified file does not exist.")
+            sys.exit()
+
+#@click.command()
+
+#def setupAccounts(setup):
+#    sa = setupaccounts
+#    sa.main()
 
 if __name__ == '__main__':
     init()
